2c1a1962a992c01f2e03964f8a67c5f17e3507d5
Kelvin <koolkt10@gmail.com>
2023-11-06 22:57:50 -0800

    string auxi function update

---
+++ string_auxi_func.c
@@ -0,0 +1,93 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of string
+ *
+ * @s: string
+ *
+ * Return: length
+ */
+
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (*(s + i) != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+/**
+ * _strcpy - pointer copies string
+ *
+ * @dest: pointer variable
+ * @src: pointer variable
+ *
+ * Return: dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int count = 0;
+
+	while (count >= 0)
+	{
+		*(dest + count) = *(src + count);
+		if (*(src + count) == '\0')
+			break;
+		count++;
+	}
+	return (dest);
+}
+
+/**
+ * _strncpy - copies string of n bytes
+ *
+ * @dest: destination variable
+ * @src: source variable
+ * @n: number of strings to copy
+ *
+ * Return: *dest
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+	for (i = i; i < n; i++)
+	{
+		dest[i] = '\0';
+	}
+
+	return (dest);
+}
+
+/**
+ * _strcmp - compares two strings
+ *
+ * @s1: first string
+ * @s2: second string
+ *
+ * Return: - 0 +
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+
+	for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
+	{
+		if (s1[i] != s2[i])
+		{
+			return (s1[i] - s2[i]);
+		}
+	}
+
+	return (0);
+}
